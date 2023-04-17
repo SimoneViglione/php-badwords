dc1fb3feb30d721ff78bf01fbe4d6b44edfd8e9f
Simone Viglione <zimoviglio51@gmail.com>
2023-04-17 16:50:39 +0200

    fine esercizio base

--- censura.php
+++ censura.php
@@ -1,3 +1,17 @@
 <?php
+    $paragrafo = $_GET['paragraph'];
+    $parolaccia = $_GET['badword'];
 
+    echo "Paragrafo non censurato:<br>";
+    echo $paragrafo . "<br>";
+    echo "Lunghezza del paragrafo in caratteri: " . strlen($paragrafo) . "<br><br>";
+
+    $paragrafo_censurato = str_ireplace($parolaccia, '***', $paragrafo);
+
+    echo "Paragrafo censurato:<br>";
+    echo $paragrafo_censurato . "<br>";
+    echo "Lunghezza del paragrafo censurato in caratteri: " . strlen($paragrafo_censurato) . "<br>"; 
 ?>
+
+
+    
--- index.php
+++ index.php
@@ -7,6 +7,17 @@
     <title>php-badwords</title>
 </head>
 <body>
+
+    <h1> Parolacce simulator </h1>
+
+    <form action="censura.php" method="get">
+        <label for="paragraph">Inizia a scrivere</label><br>
+        <input type="text" name="paragraph" id="paragraph"><br><br>
+
+        <label for="parolaccia">Digita la parola da censurare</label><br>
+        <input type="text" name="badword" id="parolaccia"><br><br>
     
+        <button type="submit"> Invia </button>
+    </form>
 </body>
 </html>
